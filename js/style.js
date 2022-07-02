725f0b25f081a7a7327c8700188caa835c74dd82
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-07-02 10:16:55 +0900

    tab 大枠完成

--- js/style.js
+++ js/style.js
@@ -1,13 +1,13 @@
-$(function () {
-  // padding内側の横幅を取得し、変数に格納
-  const w = $(".l-contents_main_article_new_img").height();
-
-  // コンソールログに表示
-  console.log(w);
-
-  const change_W = w + 80;
-
-  console.log(change_W);
-
-  jQuery(".l-contents").css("height", change_W);
+$(".c-tab-toggle").on("click", function () {
+  $(this)
+    .closest(".c-tab-container")
+    .find(".c-tab-content")
+    .removeClass("show");
+  $(this)
+    .closest(".c-tab-container")
+    .find(".c-tab-content")
+    .eq($(this).index())
+    .addClass("show");
+  $(this).closest(".c-tab-container").find(".c-tab-toggle").removeClass("on");
+  $(this).addClass("on");
 });
